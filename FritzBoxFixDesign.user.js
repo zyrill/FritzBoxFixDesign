5e669b2459bdcb76ea39d9ae2984229ddcffd2b1
zyrill <zyrill@users.noreply.github.com>
2017-09-07 15:56:57 +0200

    Create FritzBoxFixDesign.user.js

---
+++ FritzBoxFixDesign.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Freetz Design Fix
+// @namespace   https://github.com/zyrill/FritzBoxFixDesign
+// @description Adjust the Freetz design slightly
+// @author      zyrill
+// @include     http://fritz.box:81/*
+// @encoding    utf-8
+// @grant       none
+// @run-at      document-start
+// @version     1.0
+// ==/UserScript==
+
+document.getElementById("world").style.maxWidth="900px";
